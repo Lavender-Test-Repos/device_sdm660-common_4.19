11bc07b46aceab4aa4cdf6abbbf8f50fb8e1fe5b
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-03-04 00:08:59 +0530

    sdm660-common: av: Switch to qcom/common component
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
    
    s

--- sdm660.mk
+++ sdm660.mk
@@ -356,6 +356,7 @@ PRODUCT_PACKAGES += \
 
 # QTI common
 TARGET_COMMON_QTI_COMPONENTS := \
+    av \
     bt \
     telephony
 
@@ -391,7 +392,6 @@ PRODUCT_COPY_FILES += \
 # OMX
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor \
-    libavservices_minijail \
     libavservices_minijail.vendor \
     libc2dcolorconvert \
     libhypv_intercept \
